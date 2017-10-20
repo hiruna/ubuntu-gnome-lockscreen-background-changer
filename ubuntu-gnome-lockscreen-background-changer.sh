ee6df9b3beb068fc8a75e4e2c375612a246e23ea
Hiruna Wijesinghe <hiruna.kawinda@gmail.com>
2017-10-21 01:05:52 +1100

    Include bash script
    Tested with Ubuntu 17.10

---
+++ ubuntu-gnome-lockscreen-background-changer.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#title: ubuntu-gnome-lockscreen-background-changer.sh
+#description: Changes the default dark purple color from lock screen
+#author: Hiruna Wijesinghe https://github.com/hiruna/ubuntu-gnome-lockscreen-background-changer
+#date: 21/10/2017
+
+#change the value to the hex color code you like
+color=#000000
+
+#change the file path to the background image you wish to use
+image='/path/to/image.jpg'
+
+#ubuntu.css location - DO NOT CHANGE
+csslocation='/home/hiruna/testsed/ubuntu.css'
+#backup location - DO NOT CHANGE
+cssbaklocation='/home/hiruna/testsed/ubuntu.css.old'
+
+#Exit if any line fails
+set -e
+
+#if backup of ubuntu.css doesn't exist, create it
+if [ ! -e ${cssbaklocation} ] ; then
+cp ${csslocation} ${cssbaklocation}
+fi
+
+#copy contents of backup file to original
+cp ${cssbaklocation} ${csslocation}
+
+#change color and background image as specified by user
+sed -i "/background: #2c001e url(resource:\/\/\/org\/gnome\/shell\/theme\/noise-texture.png);/c\background: $color url(\"$image\");}" ${csslocation}
+
+sed -i '/background-repeat: repeat; }/c\ ' ${csslocation}
+
